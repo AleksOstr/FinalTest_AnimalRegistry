5523bdb539ff0887defda89f12b67c3d25e81f52
AleksOstr <ostryankoam@yandex.ru>
2023-05-29 22:49:23 +0400

    corrections

--- src/model/Registry.java
+++ src/model/Registry.java
@@ -2,14 +2,29 @@ package model;
 
 import Databases.PackAnimalDatabase;
 import Databases.PetDatabase;
+import model.animals.Animal;
 import model.animals.Command;
 import model.animals.PackAnimal;
 import model.animals.Pet;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Registry {
-    private PetDatabase petDB = new PetDatabase();
-    private PackAnimalDatabase packAnimalDB = new PackAnimalDatabase();
+    private PetDatabase petDB;
+    private PackAnimalDatabase packAnimalDB;
+    private Reader reader;
+
+    public Registry(String availablePetsFileName, String availablePackAnimalsFilename) {
+        this.petDB = new PetDatabase();
+        this.packAnimalDB = new PackAnimalDatabase();
+        this.reader = new Reader(availablePetsFileName, availablePackAnimalsFilename);
+    }
 
     public void showPets() {
         petDB.showDatabase();
@@ -19,12 +34,14 @@ public class Registry {
         packAnimalDB.showDatabase();
     }
 
-    private void addPet(Pet pet) {
-        petDB.add(pet);
-    }
-
-    private void addPackAnimal(PackAnimal packAnimal) {
-        packAnimalDB.add(packAnimal);
+    public void addNewAnimal(Animal animal) {
+        ArrayList<String> availablePets = reader.getAvailablePets();
+        ArrayList<String> availablePackAnimals = reader.getAvailablePackAnimals();
+        if (availablePets.contains(animal.getAnimalType())) {
+            petDB.add(new Pet(animal.getAnimalName(), animal.getAnimalType(), animal.getAnimalBirthday()));
+        } else if (availablePackAnimals.contains(animal.getAnimalType())) {
+            packAnimalDB.add(new PackAnimal(animal.getAnimalName(), animal.getAnimalType(), animal.getAnimalBirthday()));
+        } else throw new RuntimeException("We don't know this animal type");
     }
 
     public void addCommandToPet(String targetPetName, String commandName) {
@@ -71,5 +88,33 @@ public class Registry {
 
     public void showPackAnimalCommands(String packAnimalName) {
         findPackAnimalByName(packAnimalName).showCommands();
+    }
+
+    private class Reader {
+        String petsFileName;
+        String packAnimalsFileName;
+
+        public Reader(String petsFileName, String packAnimalsFileName) {
+            this.petsFileName = petsFileName;
+            this.packAnimalsFileName = packAnimalsFileName;
+        }
+
+        private String readline(String fileName) {
+            try (BufferedReader bR = new BufferedReader(new FileReader(fileName))) {
+                return bR.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException("Something went wrong...");
+            }
+        }
+
+        public ArrayList<String> getAvailablePets() {
+            List<String> stringList = Arrays.asList(readline(petsFileName).split(";"));
+            return new ArrayList<>(stringList);
+        }
+
+        public ArrayList<String> getAvailablePackAnimals() {
+            List<String> stringList = Arrays.asList(readline(packAnimalsFileName).split(";"));
+            return new ArrayList<>(stringList);
+        }
     }
 }
